8f23b21cd56323945fa6828d5c4bade484c4cb49
VictoriaSultan <86721882+VictoriaSultan@users.noreply.github.com>
2022-09-11 16:55:53 +0200

    update validation movieId

--- middlewares/validators.js
+++ middlewares/validators.js
@@ -28,7 +28,8 @@ module.exports.validateCreateMovie = celebrate({
 
 module.exports.validateMovieId = celebrate({
   params: Joi.object().keys({
-    movieId: Joi.string().hex().length(24),
+    // movieId: Joi.string().hex().length(24),
+    movieId: Joi.number().required(),
   }),
 });
 
